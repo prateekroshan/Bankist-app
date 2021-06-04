1fbcf9ba275d25ebd5113dd532c1db7d12a3e9e8
prateekroshan <prateekroshan6@gmail.com>
2021-06-04 09:16:08 +0530

    new change

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const account1 = {
 
 const account2 = {
   owner: "Jessica Davis",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  movements: [500, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
